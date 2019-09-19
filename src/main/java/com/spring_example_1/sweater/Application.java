e6dd758f2b34c2a0a0ed0f78f402b459d552fb9a
NeKoder <dimaannason@gmail.com>
2019-09-19 11:10:47 +0300

    My first Commit with simple WEB-Page

---
+++ src/main/java/com/spring_example_1/sweater/Application.java
@@ -0,0 +1,13 @@
+package com.spring_example_1.sweater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/spring_example_1/sweater/GreetingController.java
@@ -0,0 +1,26 @@
+package com.spring_example_1.sweater;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
+                           Map<String, Object> model) {
+        model.put("name", name);
+        return "greeting";
+    }
+
+    @GetMapping
+    public String index(Map<String, Object> model) {
+        model.put("link","http://localhost:8080/greeting");
+        return "index";
+    }
+
+}
